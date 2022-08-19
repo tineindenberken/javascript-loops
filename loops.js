8236388818e85a0daf5aee409ea9c2dfea7bfd9a
tineindenberken <tineevelien@hotmail.com>
2022-08-19 14:27:26 +0200

    tweede update

--- loops.js
+++ loops.js
@@ -5,9 +5,9 @@
 // Hoera!
 // Hoera!
 // ==========================================
-n
-
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
 // Verwachte uitkomsten:
@@ -17,9 +17,10 @@ n
 // loop...
 // klaar!
  // ==========================================
-
-
-
+const second = ["loop...", "loop...", "loop...", "loop...", "klaar!"]; {
+    for (let i = 0; i < second.length; i++)
+        console.log(second[i]);
+}
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -29,12 +30,15 @@ n
 // 3 is oneven
 // .... etc.
 // 15 is oneven
-
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
-
-
+for (let i = 0; i < 16; i++) {
+        if (i % 2 === 0) {
+            console.log(i + " is even")
+        } else {
+            console.log(i + " is oneven")
+        }
+}
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
 // Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
@@ -50,4 +54,9 @@ n
 // >> 8
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(">>")
 
+    }
+}
